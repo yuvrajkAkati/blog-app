684c927f2caf6d0fd331b1d5f4203fc5d8d299d1
yuvrajkAkati <139755961+yuvrajkAkati@users.noreply.github.com>
2024-09-04 01:41:52 +0530

    signin signup functionality added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,17 @@ import { Signup} from './pages/Signup'
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import {WriteBlog} from './pages/WriteBlog'
+import { LandingPage } from './pages/LandingPage'
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/" element={<LandingPage></LandingPage>}></Route>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/blog/:id" element={<Blog/>} />
           <Route path="/blogs" element={<Blogs/>} />
           <Route path='/publish' element={<WriteBlog></WriteBlog>}></ Route>
         </Routes>
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,13 +1,19 @@
 import { Quote } from "../components/Quote"
 import {useState, ChangeEvent } from "react"
-import { Link } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export const Signin = ()=>{
 
     const [name,setName] =useState("") 
     const [password,setPassword] = useState("")
     const [email,setEmail] = useState("")
+
+    const navigate = useNavigate()
+
+    function goToDashboardPage(){
+        navigate('/')
+    }
 
     return (
         <div>
@@ -31,17 +37,28 @@ export const Signin = ()=>{
                     </div>
                     <div>
                         <input type="text" className="w-full border-2 rounded h-8 pl-2 py-2" placeholder="Enter your email" onChange={(e : ChangeEvent<HTMLInputElement>)=>{
-                            setName(e.target.value)
+                            setEmail(e.target.value)
                         }}/>
                     </div>
                     <div className="font-semibold py-3">
                         Password
                     </div>
                     <div className="">
-                        <input type="text" className="w-full border-2 rounded h-8 pl-2 py-2" placeholder="Enter your password"/>
+                        <input type="text" className="w-full border-2 rounded h-8 pl-2 py-2" placeholder="Enter your password" onChange={(e)=>{
+                            setPassword(e.target.value)
+                        }}/>
                     </div>
                     <div className="text-center pt-5 ">
-                        <button className="bg-black w-full rounded h-10 text-white">
+                        <button className="bg-black w-full rounded h-10 text-white" onClick={async()=>{
+                            const response = await axios.post("http://127.0.0.1:8787/api/v1/signup",{
+                                email : email,
+                                password : password
+                            })
+                            if(response.data.token){
+                                localStorage.setItem('token',response.data.token)
+                                goToDashboardPage()
+                            }
+                        }}>
                             Sign In
                         </button>
                     </div>
